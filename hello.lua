c800f2e7f12d643da315c7f733f596fe8c1a53d7
dyu <david.yu.ftw@gmail.com>
2013-10-05 13:25:18 +0800

    add luajit

---
+++ hello.lua
@@ -0,0 +1,27 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+int plus(int x, int y);
+int plusone(int x);
+long long current_timestamp();
+
+int printf(const char *fmt, ...);
+]]
+
+-- load
+local newplus = ffi.load("newplus")
+newplus.current_timestamp()
+newplus.plusone(0)
+
+-- start
+local start = newplus.current_timestamp()
+
+local x = 0
+while x < 2000000000 do
+    x = newplus.plusone(x)
+end
+
+local endts = newplus.current_timestamp()
+local elapsed = endts - start
+
+ffi.C.printf("%lld\n", elapsed);
