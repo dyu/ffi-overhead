29ea6f3f39461394d2c51cb7bd6fee72d5658b5f
dyu <david.yu.ftw@gmail.com>
2017-02-17 12:27:33 +0800

    tup order

--- compile-all.sh
+++ compile-all.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-nim c -c -d:release --parallelBuild:1 hello.nim && \
+nim c --noLinking -d:release --parallelBuild:1 hello.nim && \
+    tup upd && \
     gcc -O2 -o nim_hello nimcache/stdlib_system.o nimcache/hello.o -Lnewplus -lnewplus -Wl,-rpath='$ORIGIN'/newplus && \
-    javac -d . jhello/Hello.java && tup upd && \
+    javac -d . jhello/Hello.java && \
     go build hello.go && \
     rustc -C opt-level=2 -C link-args="-Lnewplus -lnewplus -Wl,-rpath=\$ORIGIN/newplus" -o rust_hello hello.rs
 
