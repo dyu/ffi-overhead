b311020c1b9724ab65c721211441b77ff130ddd2
dyu <david.yu.ftw@gmail.com>
2018-05-31 21:29:19 +0800

    chain

--- compile-all.sh
+++ compile-all.sh
@@ -2,7 +2,7 @@
 
 nim c --noLinking -d:release --parallelBuild:1 hello.nim && \
     zig build -Drelease-fast=true && \
-    make -C ocaml &&
+    make -C ocaml && \
+    make -C elixir && \
     tup upd
 
-make -C elixir
