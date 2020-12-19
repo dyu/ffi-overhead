3305dc1e01ea0deb7d1307dd05d5b98e81239a3e
Tim Gates <tim.gates@iress.com>
2020-12-20 06:44:29 +1100

    docs: fix simple typo, caculate -> calculate
    There is a small typo in jhello/jhello_Hello.c, newplus/plus.c.
    
    Should read `calculate` rather than `caculate`.

--- newplus/plus.c
+++ newplus/plus.c
@@ -16,7 +16,7 @@ long long current_timestamp()
 {
     struct timeval te; 
     gettimeofday(&te, NULL); // get current time
-    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
     
     return milliseconds;
 }
